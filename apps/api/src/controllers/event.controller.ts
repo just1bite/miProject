5ea9bff80a63b60ff98437efa2eb02766aed9379
WetBananas <everlyn.frellita@gmail.com>
2024-01-19 21:41:50 +0700

    fix: event controller

--- apps/api/src/controllers/event.controller.ts
+++ apps/api/src/controllers/event.controller.ts
@@ -1,7 +1,8 @@
 // getAllJSDocTagsasd
 
+import { date, number, object, string } from 'yup';
+
 export interface eventPayload {
-  id: number;
   title: string;
   eventDescription: string;
   price: number;
@@ -9,3 +10,20 @@ export interface eventPayload {
   eventLocation: string;
   seatCount: number;
 }
+
+export const eventPayload = object({
+  body: object({
+    title: string()
+      .min(6, 'minimun lenght of title is 6')
+      .max(32, 'maximun lenght of title is 32')
+      .required('event title is required'),
+    eventDescription: string()
+      .min(16, 'minimun lenght of description is 16')
+      .max(128, 'maximun length of description is 128')
+      .required('description is required'),
+    price: number().required('price is required'),
+    eventDate: date().required('date is require'),
+    eventLocation: string().required('location is require'),
+    seatCount: number().required('available seat is require'),
+  }),
+});
